e4cc108a4a7343db4d59f5cc3c821f06f9857ce1
kelvin peñafiel <kelvinnaru12@gmail.com>
2024-03-23 19:26:39 -0500

    para crear un subidas de monedas

---
+++ app/src/main/java/com/example/android1/model/Asset.kt
@@ -0,0 +1,10 @@
+package com.example.android1.model
+
+data class Asset {
+    val id: String,
+    val name: String,
+    val symbol: String,
+    val price: String,
+    val percentage: String,
+
+}
